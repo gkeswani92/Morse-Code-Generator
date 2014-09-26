f8277b3e90766378ed4fd108591416cc7d65a4a0
Gaurav <gauravkeswani92@gmail.com>
2014-09-26 22:36:14 +0530

    Refactoring: Common method for unpacking wav files

--- src/MorseSoundToEnglish.py
+++ src/MorseSoundToEnglish.py
@@ -5,76 +5,31 @@ from MorseToEnglish import MorseToEnglish
 morse, dot, dash = [], [], []
 morse_text = ""
 
-def readMorseFile():
-    '''This function unpacks the morse code sound file and returns a list with the string 
+def unpackWaveFile(path):
+    '''This function unpacks the sound file and returns a list with the string 
     bytes of the frames that have been processed'''
     
-    waveFileMorse = wave.open(r"C:\Users\Gaurav Keswani\Documents\Eclipse\Morse-Code-Generator\src\resources\sound\morse.wav", 'r')
-    #Read the number of frames that are present in the file
-    x =waveFileMorse.readframes(waveFileMorse.getnframes())
-    
-    #Unpack the number of frames multiplied by the number of channels
-    arg = "<"+str(waveFileMorse.getnframes()*waveFileMorse.getnchannels())+"H"
-    tup_morse = struct.unpack(arg, x)
-    
-    #Since there are 2 channels, we only want one of the two duplicate entries
-    list_morse = []
-    for i in range(len(tup_morse)):
-        if waveFileMorse.getnchannels()==2:
-            if i % 2 ==0:
-                list_morse.append(tup_morse[i])
-    
-    #print ("Morse File:",list_morse)
-    return list_morse
-
-
-def readDotFile():  
-    '''This function unpacks the dot sound file and returns a list with the string 
-    bytes of the frames that have been processed'''
-    
-    waveFileDot = wave.open(r"C:\Users\Gaurav Keswani\Documents\Eclipse\Morse-Code-Generator\src\resources\sound\dot.wav", 'r')
+    file = wave.open(path, 'r')
     
     #Read the number of frames that are present in the file
-    x = waveFileDot.readframes(waveFileDot.getnframes())
+    n = file.readframes(file.getnframes())
     
     #Unpack the number of frames multiplied by the number of channels
-    arg = "<"+str(waveFileDot.getnframes()*waveFileDot.getnchannels())+"H"
-    tup_dot = struct.unpack(arg, x) 
-    
-    list_dot = []
-    
-    for i in range(len(tup_dot)):
-        if waveFileDot.getnchannels()==2:
-            if i % 2 ==0:
-                list_dot.append(tup_dot[i]) 
-    
-    #print ("Dot File:",list_dot)
-    return list_dot
+    arg = "<"+str(file.getnframes()*file.getnchannels())+"H"
+    tuple_file = struct.unpack(arg, n)
 
+    #Read elements from the tuple and add to the list filtering based on the number of channels
+    list_file = []
+    for i in range(len(tuple_file)):
+        #If number of channels is 2, then each element is repeated twice
+        if i % file.getnchannels() ==0:
+            list_file.append(tuple_file[i]) 
+    
+    return list_file
 
-def readDashFile():
-    '''This function unpacks the dash sound file and returns a list with the string 
-    bytes of the frames that have been processed'''
-    
-    waveFileDash = wave.open(r"C:\Users\Gaurav Keswani\Documents\Eclipse\Morse-Code-Generator\src\resources\sound\dash.wav", 'r')
-    
-    x =waveFileDash.readframes(waveFileDash.getnframes())
-    
-    #Unpack the number of frames multiplied by the number of channels
-    arg = "<"+str(waveFileDash.getnframes()*waveFileDash.getnchannels())+"H"
-    tup_dash = struct.unpack(arg, x)
-    
-    list_dash = []
-    for item in tup_dash:
-        list_dash.append(item)
-        
-    #print ("Dash File:", list_dash)
-    return list_dash
-    
     
 def checkForSpace(flag):
     '''This function is used to check for and thus add spaces between the morse code alphabets'''
-    
     global morse_text
     
     #If flag is true and the call has reached here, it means that all 0's are over and we have reached the next element
@@ -84,14 +39,11 @@ def checkForSpace(flag):
     return flag   
 
 
-
 def printMorseText():
     '''This function takes the morse, dot and dash byte string lists and prints out the 
     morse code in the text form'''
     
     global morse, dot, dash, morse_text   
-    
-    
     flag = False
     
     for i, item in enumerate(morse):
@@ -119,13 +71,14 @@ def printMorseText():
              
     return morse_text  
 
+
 def main():
     '''Controller function for the program'''
     global morse, dot, dash
-    
-    morse = readMorseFile()
-    dot = readDotFile()
-    dash = readDashFile()
+        
+    morse = unpackWaveFile(r"C:\Users\Gaurav Keswani\Documents\Eclipse\Morse-Code-Generator\src\resources\sound\morse.wav")
+    dot = unpackWaveFile(r"C:\Users\Gaurav Keswani\Documents\Eclipse\Morse-Code-Generator\src\resources\sound\dot.wav")
+    dash = unpackWaveFile(r"C:\Users\Gaurav Keswani\Documents\Eclipse\Morse-Code-Generator\src\resources\sound\dash.wav")
     
     morse_text = printMorseText()
     
